ed40e7948c94614564ad0a5f52606786dbcf1fb8
tailly <thibaud.tailly@gmail.com>
2020-04-08 17:21:46 +0200

    refacto: sauvegarde juste avant changement orga code

--- cvGamifie/ClientApp/src/app/app.module.ts
+++ cvGamifie/ClientApp/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AngularFileUploaderModule } from "angular-file-uploader";
 import { RouterModule } from '@angular/router';
+import { FileUploadModule } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 import { CreatePageComponent } from './pages/create-page/create-page.component';
@@ -22,16 +24,20 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FetchDataComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
     FormsModule,
-    NgbModule, 
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'create', component: CreatePageComponent},
-    ])
+    BrowserModule//.withServerTransition({ appId: 'ng-cli-universal' }),
+    //RouterModule.forRoot([
+    //  { path: '', component: HomeComponent, pathMatch: 'full' },
+    //  { path: 'counter', component: CounterComponent },
+    //  { path: 'fetch-data', component: FetchDataComponent },
+    //  { path: 'create', component: CreatePageComponent },
+    //])
+    //NgbModule
+    //HttpClientModule,
+    //AngularFileUploaderModule,
+    //FileUploadModule,
+    //NgbModule, 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cvGamifie/ClientApp/src/app/pages/create-page/create-page.component.ts
+++ cvGamifie/ClientApp/src/app/pages/create-page/create-page.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 
+const uploadAPI = 'http://localhost:4000/api/upload';
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html'
 })
-export class CreatePageComponent {
+export class CreatePageComponent implements OnInit{
   public uploader: FileUploader = new FileUploader({ url: uploadAPI, itemAlias: 'file' });
-  public currentCount = 0;
 
-  public incrementCounter() {
-    this.currentCount++;
+  ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+      console.log('FileUpload:uploaded successfully:', item, status, response);
+      alert('Article a bien été ajouté');
+    };
   }
 }
