0c301ad75db13a0b22364378a8642e3bfdd50d62
tailly <thibaud.tailly@gmail.com>
2020-04-08 07:23:24 +0200

    v1

--- cvGamifie/ClientApp/src/app/pages/create-page/create-page.component.ts
+++ cvGamifie/ClientApp/src/app/pages/create-page/create-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-create-page',
-  templateUrl: './create-page.component.css'
+  templateUrl: './create-page.component.html'
 })
 export class CreatePageComponent {
+  public uploader: FileUploader = new FileUploader({ url: uploadAPI, itemAlias: 'file' });
   public currentCount = 0;
 
   public incrementCounter() {
